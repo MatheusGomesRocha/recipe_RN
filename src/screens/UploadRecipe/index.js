ab6765b74b9eaa8b7c55a804523a0c47fe4658a4
Matheus Gomes <matheusgomes192@hotmail.com>
2021-08-25 21:29:04 -0300

    Criando Header e Area para fazer Upload da imagem da recita na tela UploadRecipe

--- src/screens/UploadRecipe/index.js
+++ src/screens/UploadRecipe/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import { TouchableNativeFeedback } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import Feather from 'react-native-vector-icons/Feather';
+
+import { defaultColor } from '../../globals';
 
 import {
     UploadRecipeContainer,
+
+    Header,
+    GoBackButton,
+    Title,
+
+    UploadImageArea,
+    UploadImageTitle,
+    UploadImageText,
 } from './styles';
 
 export default function UploadRecipe () {
+    const navigation = useNavigation();
     return(
         <UploadRecipeContainer>
+            <Header>
+                <TouchableNativeFeedback background={TouchableNativeFeedback.Ripple('#bbb', true)} onPress={() => navigation.goBack()}>
+                    <GoBackButton>
+                        <Feather name="arrow-left" color="#000" size={25}/>
+                    </GoBackButton>
+                </TouchableNativeFeedback>
 
+                <Title>Upload new recipe</Title>
+            </Header>
+
+            <UploadImageArea>
+                <Feather name="upload-cloud" size={65} color={defaultColor} />
+                <UploadImageTitle>Upload Cover</UploadImageTitle>
+                <UploadImageText>Click here to upload cover photo</UploadImageText>
+            </UploadImageArea>
         </UploadRecipeContainer>
     )
 }
--- src/screens/UploadRecipe/styles.js
+++ src/screens/UploadRecipe/styles.js
@@ -1,3 +1,47 @@
 import styled from 'styled-components/native';
+import { black, grayFont, grayish, white } from '../../globals';
 
-export const UploadRecipeContainer = styled.SafeAreaView``;
+export const UploadRecipeContainer = styled.SafeAreaView`
+    flex: 1;
+    background-color: ${white};
+    padding: 20px;
+`;
+
+/** -------------------------------------------------- **/
+
+export const Header = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+export const GoBackButton = styled.View`
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+`;
+export const Title = styled.Text`
+    font-size: 19px;
+    margin-left: 10px;
+`;
+
+/** -------------------------------------------------- **/
+
+export const UploadImageArea = styled.View`
+    align-items: center;
+    justify-content: center;
+    height: 250px;
+    background-color: ${grayish};
+    border: 1px dashed ${black};
+    border-radius: 20px;
+    margin-top: 30px;
+`;
+export const UploadImageTitle = styled.Text`
+    font-weight: bold;
+    font-size: 20px;
+    margin-top: 15px;
+`;
+export const UploadImageText = styled.Text`
+    color: ${grayFont};
+    font-size: 15px;
+    margin-top: 5px;
+`;
