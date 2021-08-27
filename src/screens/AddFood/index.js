d906aa4ea654d818f4c4838b1bff69de46524212
Matheus Gomes <matheusgomes192@hotmail.com>
2021-08-27 15:57:56 -0300

    Finalizando design da tela AddFood quando for para adicionar nova comida manualmente (com formulário para preencher)

--- src/screens/AddFood/index.js
+++ src/screens/AddFood/index.js
@@ -1,15 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
 
 import Header from '../../components/Header';
+import { black, blackish, defaultColor, white } from '../../globals';
 
 import {
     AddFoodContainer,
+
+    ActionButtons,
+    ActionButton,
+    ActionButtonText,
+
+    InputArea,
+
+    InputView,
+    Label,
+    InputFather,
+    Input,
+    QuantityItem,
+    QuantityItemText,
+
+    SubmitButton,
+    SubmitButtonText
 } from './styles';
 
+let array = [
+    {id: 1, title: 'KG'},
+    {id: 2, title: 'Piece'},
+    {id: 3, title: 'Packet'},
+    {id: 4, title: 'Others'},
+];
+
 export default function AddFood () {
+    const [addFood, setAddFood] = useState('manually');
+    const [quantityType, setQuantityType] = useState('');
+
+    const RenderItemManually = () => {
+        return(
+            <InputArea>
+                <InputView>
+                    <Label>Name</Label>
+
+                    <InputFather>
+                        <Input />
+                    </InputFather>
+                </InputView>
+
+                <InputView>
+                    <Label>Quantity</Label>
+
+                    <ScrollView horizontal={true} contentContainerStyle={{paddingHorizontal: 20, marginVertical: 10}}>
+                        {array.map((item, k) => (
+                            <QuantityItem borderColor={quantityType === item.title ? defaultColor : 'transparent'} onPress={() => setQuantityType(item.title)} key={k}>
+                                <QuantityItemText color={quantityType === item.title ? defaultColor : blackish}>{item.title}</QuantityItemText>
+                            </QuantityItem>
+                        ))}
+                    </ScrollView>
+
+                    <InputFather>
+                        <Input />
+                    </InputFather>
+                </InputView>
+
+                <InputView>
+                    <Label>Expire date</Label>
+
+                    <InputFather>
+                        <Input />
+                    </InputFather>
+                </InputView>
+
+                <SubmitButton>
+                    <SubmitButtonText>Add Food</SubmitButtonText>
+                </SubmitButton>
+            </InputArea>
+        )
+    }
+
     return(
         <AddFoodContainer>
             <Header title="Add New Food" />
+
+            <ActionButtons>
+                <ActionButton backgroundColor={addFood === 'manually' ? defaultColor : 'transparent'} onPress={() => setAddFood('manually')}>
+                    <ActionButtonText color={addFood === 'manually' ? white : black}>Add Manually</ActionButtonText>
+                </ActionButton>
+
+                <ActionButton backgroundColor={addFood === 'code' ? defaultColor : 'transparent'} onPress={() => setAddFood('code')}>
+                    <ActionButtonText color={addFood === 'code' ? white : black}>Scan QR Code</ActionButtonText>
+                </ActionButton>
+            </ActionButtons>
+
+            {addFood === 'manually' ? 
+                <RenderItemManually /> 
+                : 
+                undefined
+            }
         </AddFoodContainer>
     )
 }
--- src/screens/AddFood/styles.js
+++ src/screens/AddFood/styles.js
@@ -1,7 +1,84 @@
 import styled from 'styled-components/native';
-import { white } from '../../globals';
+import { black, defaultColor, grayFont, grayish, white } from '../../globals';
 
 export const AddFoodContainer = styled.SafeAreaView`
     flex: 1;
     background-color: ${white};
+`;
+
+/** ------------------------------------------------- */
+
+export const ActionButtons = styled.View`
+    flex-direction: row;
+    margin: 20px 20px 0 20px;
+    border: 1px solid ${defaultColor};
+    height: 52px;
+    border-radius: 10px;
+`;
+export const ActionButton = styled.TouchableOpacity`
+    background-color: ${props=>props.backgroundColor};
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+`;
+export const ActionButtonText = styled.Text`
+    color: ${props=>props.color};
+`;
+
+/** ------------------------------------------------- */
+
+export const InputArea = styled.View`
+    flex: 1;
+    margin-top: 20px;
+`;
+
+/** ------------------------------------------------- */
+
+export const InputView = styled.View`
+    margin: 10px 0;
+`;
+export const Label = styled.Text`
+    color: ${grayFont};
+    font-size: 16px;
+    margin-left: 20px;
+`;
+export const InputFather = styled.View`
+    background-color: ${grayish};
+    margin: 5px 20px 0 20px;
+    height: 52px;
+    border-radius: 10px;
+`;
+export const Input = styled.TextInput`
+    flex: 1;
+    color: ${black};
+    padding-left: 10px;
+`;
+export const QuantityItem = styled.TouchableOpacity`
+    background-color: ${grayish};
+    align-items: center;
+    justify-content: center;
+    border-color: ${props=>props.borderColor};
+    border-width: 1px;
+    height: 52px;
+    padding: 0 20px;
+    border-radius: 10px;
+    margin-right: 10px;
+`;
+export const QuantityItemText = styled.Text`
+    color: ${props=>props.color};
+`;
+
+/** ------------------------------------------------- */
+
+export const SubmitButton = styled.TouchableOpacity`
+    background-color: ${defaultColor};
+    align-items: center;
+    justify-content: center;
+    margin: 20px 20px;
+    height: 52px;
+    border-radius: 10px;
+`;
+export const SubmitButtonText = styled.Text`
+    color: ${white};
+    font-size: 16px;
 `;
