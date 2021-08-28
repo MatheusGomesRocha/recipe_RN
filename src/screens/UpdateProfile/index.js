120870f02a819b8f533f6af10ef547b05b5489f8
Matheus Gomes <matheusgomes192@hotmail.com>
2021-08-28 15:42:07 -0300

    Finalizando Design da tela UpdateProfile

--- src/screens/UpdateProfile/index.js
+++ src/screens/UpdateProfile/index.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 import Profile from '../../assets/images/profile.jpg';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import {
     UpdateProfileContainer,
@@ -12,6 +13,14 @@ import {
     
     UpdateImageArea,
     UpdateImage,
+    UpdateImageButton,
+
+    FormArea,
+    InputArea,
+    Label,
+    Input,
+    SubmitButton,
+    SubmitButtonText,
 } from './styles';
 
 export default function UpdateProfile () {
@@ -21,10 +30,35 @@ export default function UpdateProfile () {
 
             <UpdateProfileArea>
                 <UpdateImageArea>
-                    <UpdateImage source={Profile}/>
+                    {/* <UpdateImage source={Profile}/> */}
 
-                    <FontAwesome name="user" color="#aaa" size={35} />
+                    <FontAwesome name="user" color="#aaa" size={80} />
+
+                    <UpdateImageButton>
+                        <AntDesign name="camerao" color="#aaa" size={30} />
+                    </UpdateImageButton>
                 </UpdateImageArea>
+
+                <FormArea>
+                    <InputArea>
+                        <Label>Name</Label>
+                        <Input value="Matheus" />
+                    </InputArea>
+
+                    <InputArea>
+                        <Label>Email</Label>
+                        <Input value="matheusgomes192@hotmail.com" />
+                    </InputArea>
+
+                    <InputArea>
+                        <Label>User</Label>
+                        <Input value="@matheus1254" />
+                    </InputArea>
+
+                    <SubmitButton>
+                        <SubmitButtonText>Edit Changes</SubmitButtonText>
+                    </SubmitButton>
+                </FormArea>
             </UpdateProfileArea>
         </UpdateProfileContainer>
     )
--- src/screens/UpdateProfile/styles.js
+++ src/screens/UpdateProfile/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { white } from '../../globals';
+import { black, blackish, defaultColor, grayish, white } from '../../globals';
 
 export const UpdateProfileContainer = styled.SafeAreaView`
     flex: 1;
@@ -8,9 +8,69 @@ export const UpdateProfileContainer = styled.SafeAreaView`
 
 /** ---------------------------------------------------- **/
 
-export const UpdateProfileArea = styled.View``;
+export const UpdateProfileArea = styled.View`
+    align-items: center;
+    margin-top: 50px;
+`;
 
 /** ---------------------------------------------------- **/
 
-export const UpdateImageArea = styled.View``;
-export const UpdateImage = styled.Image``; 
+export const UpdateImageArea = styled.View`
+    background-color: ${grayish};
+    align-items: center;
+    justify-content: center;
+    width: 130px;
+    height: 130px;
+    border-radius: 65px;
+`;
+export const UpdateImage = styled.Image`
+    width: 130px;
+    height: 130px;
+    border-radius: 65px;
+`; 
+export const UpdateImageButton = styled.View`
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+`;
+
+/** ---------------------------------------------------- **/
+
+export const FormArea = styled.View`
+    margin-top: 30px;
+    width: 100%;
+`;
+export const InputArea = styled.View`
+    margin-top: 20px;
+    padding: 0 20px;
+`;
+export const Label = styled.Text`
+    color: ${blackish};
+    font-size: 16px;
+`;
+export const Input = styled.TextInput`
+    background-color: ${grayish};
+    color: ${black};
+    height: 52px;
+    padding-left: 10px;
+    margin-top: 10px;
+    border-radius: 10px;
+`;
+export const SubmitButton = styled.TouchableOpacity`
+    background-color: ${defaultColor};
+    align-items: center;
+    justify-content: center;
+    height: 52px;
+    border-radius: 10px;
+    margin: 30px 20px;
+`;
+export const SubmitButtonText = styled.Text`
+    color: ${white};
+    font-size: 16px;
+`;
