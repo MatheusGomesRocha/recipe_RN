1faf6d7f04d9e2c4d8a52d708bd010ec892f770c
Matheus Gomes <matheusgomes192@hotmail.com>
2021-08-29 17:55:50 -0300

    Finalizando design da tela Notification

--- src/screens/Notification/styles.js
+++ src/screens/Notification/styles.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components/native';
+import { grayFont } from '../../globals';
 
 export const NotificationContainer = styled.SafeAreaView``;
+
+/** ----------------------------------------------------- **/
+
+export const NotificationItem = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding: 20px;
+    border-bottom-width: 1px;
+    border-bottom-color: ${grayFont};
+`;
+export const NotificationImg = styled.Image`
+    width: 80px;
+    height: 80px;
+    border-radius: 40px;
+`;
+export const NotificationContentText = styled.Text`
+    width: 70%;
+    margin-left: 15px;
+`;
