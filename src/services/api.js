5b31c56cd01ebf06a91a417a211658bb966bf4b5
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-03 09:49:39 -0300

    Aplicando prefixo para acessar a API /api/v1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://192.168.0.110:3000'
+    baseURL: 'http://192.168.0.110:3000/api/v1'
 });
 
 // 192.168.0.110  --> API 
